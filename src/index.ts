74c18ba6e0bb198a6b179c66701cf6b900b31092
mateuszrymar <118372766+mateuszrymar@users.noreply.github.com>
2023-03-13 17:04:15 +0100

    WIP File upload

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 import './index.css'
+
+
+const addUploadListener = () => {
+  const uploadButton = document.querySelector('.upload-page__button');
+  const uploadInput: HTMLInputElement | null = document.querySelector('.upload-page__input');
+  
+  uploadButton?.addEventListener('click', (event) => {uploadInput?.click()})
+}
+
+const init = (() => {
+  addUploadListener();  
+})();
