a45dadf4f209ebe6db5169ac83f94c26bb6c79fe
mateuszrymar <118372766+mateuszrymar@users.noreply.github.com>
2023-03-14 18:09:29 +0100

    Passing data between pages works now.

---
+++ src/results.ts
@@ -0,0 +1,35 @@
+import { cloneDeep } from 'lodash-es';
+
+import { displayResults } from './modules/displayResults';
+
+
+interface FrameInt {
+  frameId: number;
+  rolls: number[];
+  isStrike: boolean;
+  isSpare: boolean;
+  pointResult: number;
+}
+
+interface PlayerInt {
+  name: string;
+  result: number | null;
+  frames: FrameInt[];
+}
+
+console.log('This should work on table page only.');
+
+const getDataFromSessionStorage = () => {
+  const data = window.sessionStorage.getItem( 'playerResults' );
+  let playerData: PlayerInt[] = [];
+
+  if ( data !== null ) {
+    playerData = JSON.parse( data );
+  }
+
+  return playerData;
+}
+
+const playerData = getDataFromSessionStorage();
+
+console.log(playerData);
