2627c832489b09a1e1bbdf279a73c47eefef45bf
mateuszrymar <118372766+mateuszrymar@users.noreply.github.com>
2023-03-14 13:44:10 +0100

    Added point limit checks

--- src/modules/calculateResults.ts
+++ src/modules/calculateResults.ts
@@ -1,4 +1,4 @@
-import { cloneDeep, isEmpty } from 'lodash-es';
+import { cloneDeep, isEmpty, sum } from 'lodash-es';
 
 /*
   This module calculates the results in a similar way to how a person would do it:
@@ -39,20 +39,57 @@ export const isAnyFrameFilled = ( frames :number[][] ) => {
   else return true;
 };
 
-export const isAnyFrameOverPointLimit = ( frames :number[][] ) => {
-  console.log('testing!');
-  
-  // const framesToCheck = cloneDeep( frames );
-  // console.log('checked',framesToCheck);  
+const isLastFrameOverLimit = ( lastFrame: number[] ) => {
+  const frameToCheck = cloneDeep( lastFrame );
+  let isOverLimit = false;
 
-  return true;
+  if ((frameToCheck[0] !== undefined) && (frameToCheck[0] > 30)) {
+    console.log( 'Wrong input: Frame #10 is over 30 points.');
+    isOverLimit = true;
+  } 
+
+  return isOverLimit;
+}
+
+const isFrameOverLimit = ( regularFrames: number[] ) => {
+  const frameSums = cloneDeep( regularFrames );
+  let isOverLimit = false;
+
+  frameSums.forEach(element => {
+    if ( element > 10 ) {
+      console.log('Wrong input: one of the frames #1-9 is over 10 points.');
+      isOverLimit = true;      
+    }
+  });
+
+  return isOverLimit;
+}
+
+export const isAnyFrameOverPointLimit = ( frames :number[][] ) => {    
+  const framesToCheck = cloneDeep( frames );
+
+  // We sum rolls in each frame (without bonus points):
+  const frameSums = framesToCheck.map( frame => sum(frame));
+  const lastFrame = frameSums.splice(9);
+
+  const isFrameOverPointLimit = isFrameOverLimit( frameSums );  
+  const isLastFrameOverPointLimit = isLastFrameOverLimit( lastFrame );
+  const isOverLimit: boolean = (isLastFrameOverPointLimit || isFrameOverPointLimit);
+
+  return isOverLimit;
 };
+
+export const isAnyFrameTooLong = ( frames :number[][] ) => {
+  
+}
 
 export const validateFrames = ( frames :number[][] ) => {
   const framesToCheck = cloneDeep( frames );
-  console.log('validation is running.')
+  console.log('validation is running.');
+
   if ( isAnyFrameFilled( framesToCheck ) === false ) return false
-  if ( isAnyFrameOverPointLimit( framesToCheck ) === false ) return false
+  if ( isAnyFrameOverPointLimit( framesToCheck ) === true ) return false
+  // Now check if there wasn't too many entries
   else return true;
 };
 
--- src/tests/isAnyFrameOverPointLimit.test.ts
+++ src/tests/isAnyFrameOverPointLimit.test.ts
@@ -1,14 +1,23 @@
 import { describe, expect, it } from 'vitest';
-import { isAnyFrameFilled } from "../modules/calculateResults";
+import { isAnyFrameOverPointLimit } from "../modules/calculateResults";
 
-describe('#isAnyFrameFilled', () => {
-  it('returns false with an empty array as input', () => {
-    expect(isAnyFrameFilled([])).toStrictEqual(false);
+const correctInput_118 = [[1, 9], [1, 9], [1, 9], [1, 9], [1, 9], [1, 9], [1, 9], [1, 9], [1, 9], [1, 9, 9]];
+const wrongInputSumAboveTen = [[7, 4], [9, 1], [10], [10], [8, 2], [7, 3], [5, 5], [6, 3], [10], [10, 7, 2]]; // Expected: Error due to wrong sum in pair
+
+describe('#isAnyFrameOverPointLimit', () => {
+  it('returns false given a frame with sum of less than 11', () => {
+    expect(isAnyFrameOverPointLimit([[5,4]])).toStrictEqual(false);
   });
-  it('returns false with empty arrays in an array as input', () => {
-    expect(isAnyFrameFilled([[], []])).toStrictEqual(false);
+  it('returns true given a frame with sum of more than 10', () => {
+    expect(isAnyFrameOverPointLimit([[5,6]])).toStrictEqual(true);
   });
-  it('returns true with a filled array as input', () => {
-    expect(isAnyFrameFilled([[10]])).toStrictEqual(true);
+  it('returns true given a frame with an item over 10', () => {
+    expect(isAnyFrameOverPointLimit([[11]])).toStrictEqual(true);
+  });
+  it('returns false given a valid array', () => {
+    expect(isAnyFrameOverPointLimit( correctInput_118 )).toStrictEqual(false);
+  });
+  it('returns true given an array where sum is above 10', () => {
+    expect(isAnyFrameOverPointLimit( wrongInputSumAboveTen )).toStrictEqual(true);
   });
 });
